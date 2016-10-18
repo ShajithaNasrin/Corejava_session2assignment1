d3047c5e92671c076d279a2283b65b4e4d1e3569
ShajithaNasrin <shajitha.msn@gmail.com>
2016-10-18 13:50:09 +0530

    Add files via upload

---
+++ DisplayName.java
@@ -0,0 +1,7 @@
+class DisplayName
+{
+	public static void main(String[] args)
+	{
+		if(System.out.printf("Shajitha nasrin M")!=null){}   /*println statement we cant use in 									condition if return type is 									        void*/
+	}
+}
